2538fc311457e4c360b95046cc08b2d42fe75b8a
Kavitha Krishnappa <contact@kavitha.app>
2022-03-09 03:32:27 +0100

    tags space

--- pages/tags/[tag].js
+++ pages/tags/[tag].js
@@ -56,28 +56,32 @@ export default function Tag({ posts, projects, tag }) {
         title={`${tag} - ${siteMetadata.author}`}
         description={`${tag} tags - ${siteMetadata.author}`}
       />
-      <h1 className="text-center text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-        #{title}
-      </h1>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <h1 className="pt-10 pb-10 text-center text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          #{title}
+        </h1>
 
-      <h2 className="mt-10 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-        Projects
-      </h2>
-      {!projects.length && 'No projects found.'}
-      <div className="container py-12">
-        <div className="-m-4 flex flex-wrap">
-          {projects.map((d) => (
-            <Card
-              key={d.title}
-              title={d.title}
-              description={d.description}
-              imgSrc={d.images[0]}
-              href={`/projects/${d.slug}`}
-            />
-          ))}
+        <div>
+          <h2 className="mt-10 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Projects
+          </h2>
+          {projects.length === 0 && 'No projects found.'}
+          <div className="container py-12">
+            <div className="-m-4 flex flex-wrap">
+              {projects.map((d) => (
+                <Card
+                  key={d.title}
+                  title={d.title}
+                  description={d.description}
+                  imgSrc={d.images[0]}
+                  href={`/projects/${d.slug}`}
+                />
+              ))}
+            </div>
+          </div>
         </div>
+        <ListLayout posts={posts} title="Posts" />
       </div>
-      <ListLayout posts={posts} title="Posts" />
     </>
   )
 }
