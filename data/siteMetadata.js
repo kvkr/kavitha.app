66ee120b1e02c036d47ce04a517e6b4b69efcf2e
Kavitha Krishnappa <contact@kavitha.app>
2022-04-13 12:28:32 +0200

    description

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   mastheadImage: '/static/images/avatar.png',
   mastheadTitle: 'Hi!',
   mastheadDescription:
-    'I am into designing meaningful experiences using immersive user-centric approach.',
+    ' I enjoy UX Research, listen to human stories & build meaningful experiences. Numbers tell stories too, Data visualisation is my hobby.',
   language: 'en-us',
   siteUrl: 'https://kavitha.app',
   siteRepo: 'https://github.com/kvkr/kavitha.app',
