5d94d30efe7f2de03792570a88e6d2a4ee7f971f
Kavitha Krishnappa <contact@kavitha.app>
2022-04-13 12:42:23 +0200

    description

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   mastheadImage: '/static/images/avatar.png',
   mastheadTitle: 'Hi!',
   mastheadDescription:
-    ' I enjoy listening to human stories through UX Research. Numbers can tell stories too, Data visualisation is my hobby.',
+    ' I enjoy listening to human stories through UX Research. /n Numbers can tell stories too, Data visualisation is my hobby.',
   language: 'en-us',
   siteUrl: 'https://kavitha.app',
   siteRepo: 'https://github.com/kvkr/kavitha.app',
